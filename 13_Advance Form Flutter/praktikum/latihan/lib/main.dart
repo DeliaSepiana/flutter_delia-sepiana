83142c2a167f550c3f9169bba06bc93566bb2301
DeliaSepiana <142206551+DeliaSepiana@users.noreply.github.com>
2023-09-25 09:08:15 +0800

    submit

--- 13_Advance Form Flutter/praktikum/latihan/lib/main.dart
+++ 13_Advance Form Flutter/praktikum/latihan/lib/main.dart
@@ -1,40 +1,306 @@
 import 'package:flutter/material.dart';
-import 'combine.dart';
-import 'home.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const ContactApp());
 }
 
-class MyApp extends StatelessWidget {
+class ContactApp extends StatelessWidget {
+  const ContactApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Color(0xFF6750A4),
+      home: const ContactPage(),
+    );
+  }
+}
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({Key? key}) : super(key: key);
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  DateTime _dueDate = DateTime.now();
+  Color _currentColor = Colors.orange;
+  String _selectedFilePath = '';
+  String _submittedName = '';
+  String _submittedPhone = '';
+  DateTime _submittedDate = DateTime.now();
+  Color _submittedColor = Colors.orange;
+  String _submittedFilePath = '';
+
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+
+  void deleteContact(int index) {
+    setState(() {
+      contactList.removeAt(index);
+    });
+  }
+
+  void _pickFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+
+    final file = result.files.first;
+    setState(() {
+      _selectedFilePath = file.path!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact'),
+        backgroundColor: const Color(0xFF6200EE),
+        centerTitle: true,
       ),
-      home: DefaultTabController(
-        length: 2, // Jumlah tab (Calender dan Contacts)
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('My App'),
-            backgroundColor: Color(0xFF6750A4),
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Calender'), // Nama tab pertama
-                Tab(text: 'Contacts'), // Nama tab kedua
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.phone_android,
+              size: 25,
+            ),
+            const SizedBox(height: 18),
+            const Text(
+              'Create New Contact',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 18),
+            // Form Name
+            TextFormField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                labelText: 'Name',
+                hintText: 'Insert your name',
+                filled: true,
+                fillColor: Color.fromARGB(255, 186, 185, 185),
+                labelStyle: TextStyle(color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 12),
+            // Form Nomor HP
+            TextFormField(
+              controller: phoneController,
+              decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                hintText: '08...',
+                filled: true,
+                fillColor: Color.fromARGB(255, 186, 185, 185),
+                labelStyle: TextStyle(color: Colors.black),
+              ),
+            ),
+            const SizedBox(height: 12),
+            // Date Picker
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Date'),
+                TextButton(
+                  onPressed: () async {
+                    final selectedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1991),
+                      lastDate: DateTime(DateTime.now().year + 5),
+                    );
+
+                    if (selectedDate != null) {
+                      setState(() {
+                        _dueDate = selectedDate;
+                      });
+                    }
+                  },
+                  child: const Text('Select'),
+                ),
               ],
             ),
-          ),
-          body: TabBarView(
-            children: [
-              ContactPage(),
-              HomePage(),
-            ],
-          ),
+            Text(
+              DateFormat('dd-MM-yyyy').format(_dueDate),
+            ),
+            const SizedBox(height: 12),
+            // Color Picker
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Color'),
+                const SizedBox(height: 10),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  color: _currentColor,
+                ),
+                const SizedBox(height: 10),
+                Center(
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(_currentColor),
+                    ),
+                    child: const Text('Pick Color'),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text('Pick Your Color'),
+                            content: BlockPicker(
+                              pickerColor: _currentColor,
+                              onColorChanged: (color) {
+                                setState(() {
+                                  _currentColor = color;
+                                });
+                              },
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text('Save'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            // File Picker
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('File'),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: _pickFile,
+                  child: const Text('Pick and Open File'),
+                ),
+                Text(_selectedFilePath),
+              ],
+            ),
+            const SizedBox(height: 12),
+            // Submit Button
+            ElevatedButton(
+              onPressed: () {
+                final name = nameController.text;
+                final phoneNumber = phoneController.text;
+
+                final contact = Contact(
+                  name: name,
+                  phone: phoneNumber,
+                  date: _dueDate,
+                  color: _currentColor,
+                );
+
+                setState(() {
+                  contactList.add(contact);
+                });
+
+                nameController.clear();
+                phoneController.clear();
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Contact submitted successfully'),
+                  ),
+                );
+              },
+              child: const Text('Submit'),
+            ),
+
+            const SizedBox(height: 12),
+            // List View Contact
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: contactList.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    return const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text(
+                        'List Contact',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+                  } else {
+                    final contact = contactList[index - 1];
+                    final avatarText = contact.name[0].toUpperCase();
+
+                    return ListTile(
+                      leading: CircleAvatar(
+                        child: Text(avatarText),
+                        backgroundColor: contact.color,
+                      ),
+                      title: Text(contact.name),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Phone: ${contact.phone}'),
+                          Text(
+                              'Date: ${DateFormat('dd-MM-yyyy').format(contact.date)}'),
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.edit),
+                            onPressed: () {
+                              //  edit
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete),
+                            onPressed: () {
+                              deleteContact(index - 1);
+                            },
+                          ),
+                        ],
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+class Contact {
+  final String name;
+  final String phone;
+  final DateTime date;
+  final Color color;
+
+  Contact({
+    required this.name,
+    required this.phone,
+    required this.date,
+    required this.color,
+  });
+}
+
+List<Contact> contactList = [];
