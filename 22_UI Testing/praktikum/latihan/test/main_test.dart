22ee28ce1c6243cf1d7f541f43d6984bfd1d51ca
DeliaSepiana <142206551+DeliaSepiana@users.noreply.github.com>
2023-10-15 21:29:50 +0800

    submit

--- 22_UI Testing/praktikum/latihan/test/main_test.dart
+++ 22_UI Testing/praktikum/latihan/test/main_test.dart
@@ -4,44 +4,33 @@ import 'package:latihan/main.dart';
 
 void main() {
   testWidgets('Testing Rendering', (WidgetTester tester) async {
-   
     await tester.pumpWidget(const ContactApp());
 
-    
     expect(find.text('Contact'), findsOneWidget);
 
-
-    await tester.enterText(find.byKey(Key('nameField')), 'John Doe');
+    await tester.enterText(find.byKey(Key('nameField')), 'Delia Sepianaa');
     await tester.enterText(find.byKey(Key('phoneField')), '1234567890');
 
-    
     await tester.tap(find.text('Select'));
     await tester.pumpAndSettle();
 
-  
     await tester.tap(find.text('OK'));
     await tester.pumpAndSettle();
 
-    
     await tester.tap(find.text('Pick Color'));
     await tester.pumpAndSettle();
 
-   
     await tester.tap(find.text('Save'));
     await tester.pumpAndSettle();
 
-    
     await tester.tap(find.text('Pick a File'));
     await tester.pumpAndSettle();
 
-   
     expect(find.text('John Doe'), findsOneWidget);
 
-    
     await tester.tap(find.byIcon(Icons.delete).first);
     await tester.pumpAndSettle();
 
-  
-    expect(find.text('John Doe'), findsNothing);
+    expect(find.text('Delia Sepiana'), findsNothing);
   });
 }
